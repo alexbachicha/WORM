0497104114875e8e6ee2a7d12ff81f2d479a452d
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-23 13:32:19 -0800

    initialized js file for the modal

---
+++ public/js/modal.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+
+    // $("#myModal").modal('show');
+
+    // $(".modal").on("hidden.bs.modal", function () {
+    //     window.location = "https://www.google.com/";
+    // });
+    
+});
